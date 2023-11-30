9eca4e698d1a5b244873103dd01467e7cdbebbcb
Oleg Ershov <oecorango@gmail.com>
2023-11-30 21:00:54 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App(): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
